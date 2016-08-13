2b71e7633dc1b07b427f9defc4d28b0457aa9e8c
hernanofx <hernanofx@hotmail.com>
2016-08-13 15:17:16 -0300

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   get '/messages' => 'messages#index'
   get '/messages/new' => 'messages#new'
-  post 'messages'=> 'messages#create'
+  post '/messages'=> 'messages#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
